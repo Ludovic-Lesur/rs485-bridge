31aefc9b9a96c5c107a6663059e7e86a12464a6e
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2024-12-15 10:51:00 +0100

    Fix Git version script.

--- script/git_version.sh
+++ script/git_version.sh
@@ -19,7 +19,7 @@ date=`date -R`
 
 # Execute git commands.
 git_version=`git describe --long --always`
-diff=`git diff ':(exclude)../application/inc/mode.h' ':(exclude)../script'`
+diff=`git diff ':(exclude)../application/inc/dim_flags.h' ':(exclude)../script'`
 
 # Extract fields
 sscanf $git_version "sw([0-9]+).([0-9]+)-([0-9]+)-g(.+)"
