c3f326d490ec17197bdffaa7850f31f2cc0e3682
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2025-03-18 19:38:28 +0100

    Clean error bases file.

--- application/inc/error_base.h
+++ application/inc/error_base.h
@@ -9,20 +9,12 @@
 #define __ERROR_BASE_H__
 
 // Peripherals.
-#include "adc.h"
-#include "flash.h"
 #include "iwdg.h"
 #include "lptim.h"
-#include "lpuart.h"
-#include "nvm.h"
 #include "rcc.h"
 #include "rtc.h"
-#include "tim.h"
-#include "usart.h"
 // Utils.
-#include "math.h"
-#include "parser.h"
-#include "string.h"
+#include "error.h"
 // Middleware.
 #include "analog.h"
 #include "cli.h"
@@ -37,22 +29,12 @@
 typedef enum {
     SUCCESS = 0,
     // Peripherals.
-    ERROR_BASE_ADC1 = 0x0100,
-    ERROR_BASE_FLASH = (ERROR_BASE_ADC1 + ADC_ERROR_BASE_LAST),
-    ERROR_BASE_IWDG = (ERROR_BASE_FLASH + FLASH_ERROR_BASE_LAST),
+    ERROR_BASE_IWDG = ERROR_BASE_STEP,
     ERROR_BASE_LPTIM = (ERROR_BASE_IWDG + IWDG_ERROR_BASE_LAST),
-    ERROR_BASE_LPUART = (ERROR_BASE_LPTIM + LPTIM_ERROR_BASE_LAST),
-    ERROR_BASE_NVM = (ERROR_BASE_LPUART + LPUART_ERROR_BASE_LAST),
-    ERROR_BASE_RCC = (ERROR_BASE_NVM + NVM_ERROR_BASE_LAST),
+    ERROR_BASE_RCC = (ERROR_BASE_LPTIM + LPTIM_ERROR_BASE_LAST),
     ERROR_BASE_RTC = (ERROR_BASE_RCC + RCC_ERROR_BASE_LAST),
-    ERROR_BASE_TIM_CALIBRATION = (ERROR_BASE_RTC + RTC_ERROR_BASE_LAST),
-    ERROR_BASE_USART_AT = (ERROR_BASE_TIM_CALIBRATION + TIM_ERROR_BASE_LAST),
-    // Utils.
-    ERROR_BASE_MATH = (ERROR_BASE_USART_AT + USART_ERROR_BASE_LAST),
-    ERROR_BASE_PARSER = (ERROR_BASE_MATH + MATH_ERROR_BASE_LAST),
-    ERROR_BASE_STRING = (ERROR_BASE_PARSER + PARSER_ERROR_BASE_LAST),
     // Middleware.
-    ERROR_BASE_ANALOG = (ERROR_BASE_STRING + STRING_ERROR_BASE_LAST),
+    ERROR_BASE_ANALOG = (ERROR_BASE_RTC + RTC_ERROR_BASE_LAST),
     ERROR_BASE_CLI = (ERROR_BASE_ANALOG + ANALOG_ERROR_BASE_LAST),
     ERROR_BASE_POWER = (ERROR_BASE_CLI + CLI_ERROR_BASE_LAST),
     // Nodes.
